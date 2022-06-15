6af3051c577658ba1ba58ce0e508c0096911ecaa
ash <58450809+ashykitty@users.noreply.github.com>
2022-06-15 18:59:05 +0000

    now it receives input from stdin and automatically detects meow text

--- meow.c
+++ meow.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -5,7 +7,6 @@
 /* meow translator 10/09/21 */
 
 #define WORD_SIZE 8
-#define INVALID_MSG "INVALID MEOW TEXT OWO"
 
 static const char * const MEOWS[] = {
 	"meow",
@@ -26,32 +27,18 @@ static const char * const MEOWS[] = {
 	"mEOw"
 };
 
-char *join( int argc, char ** argv, size_t *arg_len)
+int invalid_character( char c)
 {
+	char *valid = "meowMEOW-?";
 	int i;
-	size_t full_len = 0;
-	char *text = NULL;
-	
-	for( i = 0; i < argc; i++)
-		full_len += strlen( argv[i]) + 1;
-	
-	text = malloc( full_len + 1);
-	if( text == NULL) return NULL;
-	
-	text[full_len] = 0x00;
-	
-	for( i = 0; i < argc; i++)
+
+	for( i = 0; i < strlen(valid); i++)
 	{
-		size_t len = strlen( argv[i]);
-		memcpy( text, argv[i], len);
-		*(text + len) = ' ';
-		text += len + 1;
+		if( c == valid[i])
+			return 0;
 	}
-	
-	text -= full_len;
-	*arg_len = full_len;
-	
-	return text;
+
+	return 1;
 }
 
 int match_meow( char *word)
@@ -62,95 +49,106 @@ int match_meow( char *word)
 	return -1;
 }
 
-void decrypt( int argc, char ** argv)
+int valid_meow( char *str)
+{
+	int i, words = 0;
+	char word[WORD_SIZE];
+	char *ptr = word;
+
+	memset( word, 0, WORD_SIZE);
+
+	for( i = 0; i < strlen( str); i++)
+	{
+		if( str[i] == ' ')
+		{
+			
+			if( match_meow( word) < 0)
+				return 0;
+
+			memset( word, 0, WORD_SIZE);
+			ptr = word;
+			words++;
+
+		} else {
+
+			if( invalid_character( str[i]))
+				return 0;
+
+			*(ptr++) = str[i];
+
+			if( ptr >= word+WORD_SIZE)
+				return 0;
+		}	
+
+	}
+	return words % 2 == 0;
+}
+
+void decrypt( char *str)
 {
 	int i;
-	char *text = NULL;
-	size_t len = 0;
 	
 	char word[WORD_SIZE];
 	char *ptr = word;
 	char chr = 0;
 	
-	text = join( argc, argv, &len);
 	memset( word, 0, WORD_SIZE);
 	
-	for( i = 0; i < len; i++)
+	for( i = 0; i < strlen( str); i++)
 	{
-		if( text[i] == ' ')
+		if( str[i] == ' ')
 		{
 			int meow = match_meow( word);
-			if( meow < 0)
-			{
-				printf(INVALID_MSG);
-				free( text); text = NULL;
-				return;
-			}
 			
-			if( chr > 0)
+			if( chr)
 			{
-				chr += meow;
-				printf("%c", chr);
+				printf("%c", chr+meow);
 				chr = 0;
 			} else {
-				chr += meow << 4;
+				chr = meow << 4;
 			}
 			
 			memset( word, 0, WORD_SIZE);
 			ptr = word;
 			
 		} else {
-			*(ptr++) = text[i];
-			if( ptr >= word + WORD_SIZE)
-			{
-				printf(INVALID_MSG);
-				return;
-			}			
+			*(ptr++) = str[i];
 		}
-	}
-	
-	free( text); text = NULL;
+	}	
 }
 
-void encrypt( int argc, char ** argv)
+void encrypt( char *str)
 {
-	char *text = NULL;
-	size_t len = 0;
 	int i;
-	
-	text = join( argc, argv, &len);
-	
-	for( i = 0; i < len-1; i++)
+		 
+	for( i = 0; i < strlen( str); i++)
 	{
-		char c = text[i] < 128 ? text[i] : '?';
+		char c = str[i] < 128 ? str[i] : '?';
 		printf("%s %s ", MEOWS[c>>4], MEOWS[c&15]);
 	}
+}
+
+void remove_newline( char *str)
+{
+	int i;
+	for( i = 0; i < strlen( str); i++)
+		if( str[i] == '\n')
+			str[i] = ' ';
+}
+
+int main()
+{
+	char *input = NULL;
+	size_t len = 0;
+
+	while( getline( &input, &len, stdin) > 0)
+	{
+		remove_newline( input);
+		
+		valid_meow( input) ? decrypt( input) : encrypt( input);
+	}
 	
-	free( text); text = NULL;
-}
-
-void parse_argument( int argc, char ** argv)
-{
-	if( strcmp( argv[1], "-e") == 0)
-	{
-		encrypt( argc - 2, argv + 2);
-		return;
-	}
-	if( strcmp( argv[1], "-d") == 0)
-	{
-		decrypt( argc - 2, argv + 2);
-		return;
-	}
-}
-
-int main( int argc, char ** argv)
-{
-	if( argc > 2)
-	{
-		parse_argument( argc, argv);
-	} else {
-		printf("how to use:\n\n	-e [msg]\n\n-d [msg]\n");
-	}
+	free( input); input = NULL;
 	return 0;
 }
 
